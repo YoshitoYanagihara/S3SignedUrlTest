3cd6bdee855372d579a190024e022edfbf861f59
YoshitoYanagihara <yos_yanagihara@yahoo.co.jp>
2022-06-10 01:37:54 +0900

    署名付きURL生成APIを叩く処理を実装

--- frontend/src/components/PathList.vue
+++ frontend/src/components/PathList.vue
@@ -6,11 +6,12 @@
                 <th></th><th>パス</th><th>ファイル数</th>
             </tr>
             <tr v-for="item in paths" :key="item.path">
-                <td><input type="checkbox" />
+                <td><input type="checkbox" ref="gens" :value="item.path" />
                 <td>{{ item.path }}</td>
                 <td>{{ item.files }}</td>
             </tr>
         </table>
+        <button @click="generateSignedUrl">署名付きURL発行</button>
     </div>
 </template>
 
@@ -42,6 +43,33 @@ export default {
                     }
                     this.paths.push(item)
                 })
+            } catch (error) {
+                alert('通信エラー')
+                console.error(error)
+            }
+        },
+        /**
+         * 署名付きURL発行
+         */
+        async generateSignedUrl() {
+            const paths = []
+            this.$refs.gens.forEach(chk => {
+                if (chk.checked) {
+                    paths.push(chk.value)
+                }
+            })
+            if (paths.length === 0) { return }
+
+            try {
+                const data = {
+                    paths: paths,
+                }
+                const response = await connection.post('/api/file/gen', data)
+                if (response.data.result) {
+                    alert('署名付きURLを生成しました')
+                } else {
+                    alert('署名付きURLの生成に失敗しました')
+                }
             } catch (error) {
                 alert('通信エラー')
                 console.error(error)
