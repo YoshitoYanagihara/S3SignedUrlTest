58723fa2ea8bef5b84fce6e661df5668faa10fcb
YoshitoYanagihara <yos_yanagihara@yahoo.co.jp>
2022-06-09 23:36:21 +0900

    適当にメッセージを返すだけのAPIを作成

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
-console.log("Test")
+const express = require('express')
+const app = express()
+
+const server = app.listen(3000, () => {
+    console.log('Start Server!')
+})
+
+app.get('/api/ping', (req, res) => {
+    const params = {
+        message: 'Hello',
+    }
+    res.json(params)
+})
