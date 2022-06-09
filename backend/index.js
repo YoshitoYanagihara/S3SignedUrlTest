5d5c25696d6980105992e11ab26e5d52766b42e7
YoshitoYanagihara <yos_yanagihara@yahoo.co.jp>
2022-06-09 23:57:29 +0900

    とりあえずpingを間借りしてバケットリストを返すようにしてみた

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,29 @@
+// expressサーバ
 const express = require('express')
 const app = express()
+
+// AWS関係
+const AWS = require('aws-sdk')
+const secret = require('./secret.json')
+const s3 = new AWS.S3({
+    apiVersion: '2006-03-01'
+})
 
 const server = app.listen(3000, () => {
     console.log('Start Server!')
 })
 
+// 動作テスト用のping
 app.get('/api/ping', (req, res) => {
-    const params = {
-        message: 'Hello',
-    }
-    res.json(params)
+    s3.listBuckets((err, data) => {
+        if (err) {
+            const params = {
+                error: err,
+            }
+            res.json(params)
+            return
+        }
+
+        res.json(data)
+    })
 })
